50523b5525ef5f39c306b66355d83936cbe10e89
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-01-28 02:05:13 +0100

    Update 65.ads

--- 65.ads
+++ 65.ads
@@ -11,10 +11,10 @@
 			"BannerFacebook": "628915755243490_628921531909579",
 			"InterstitialFacebook": "628915755243490_628921761909556",
 			
-			"unityGameID": "5111343",
+			"unityGameID": "5138954",
 			"UnityPlacement": "Interstitial_Android",
 
-			"StartappAds": "200325670",
+			"StartappAds": "200562820",
 
 			"Cpa": false,
 			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
